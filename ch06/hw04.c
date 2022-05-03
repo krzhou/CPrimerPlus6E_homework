54526cc67f1dfced16b2b60edfdd69d2905f7864
krzhou <kr_zhou@outlook.com>
2022-05-03 16:39:20 +0800

    update chapter06 homework04

--- ch06/hw04.c
+++ ch06/hw04.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-    char ALPHABET[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const char ALPHABET[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     int curr_index = 0;
     for (int row = 0; row < 6; row++) {
         for (int col = 0; col <= row; col++, curr_index++) {
